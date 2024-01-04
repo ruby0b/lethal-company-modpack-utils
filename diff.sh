712e4d4284e2a1bac332b8cd8f1178968179e65b
ruby0b <106119328+ruby0b@users.noreply.github.com>
2024-01-04 23:16:59 +0100

    support different modpack directories

--- diff.sh
+++ diff.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # Compare the r2modman dependency string list (clipboard) with those of the modpack.
 
+modpack=${1:-modpack}
+
 diff --color=always \
-    <(jq <modpack/manifest.json '.dependencies.[]' | cut -c2- | rev | cut -c2- | rev | sort) \
+    <(jq <"$modpack/manifest.json" '.dependencies.[]' | cut -c2- | rev | cut -c2- | rev | sort) \
     <(xclip -selection clipboard -o | sort)
--- release.sh
+++ release.sh
@@ -1,27 +1,32 @@
 #!/usr/bin/env sh
 set -e
 
-version() { sed -n 's/\s*"version_number":\s*"\(.*\)".*/\1/p' modpack/manifest.json; }
+modpack=${1:-modpack}
+
+version() { sed -n 's/\s*"version_number":\s*"\(.*\)".*/\1/p' "$modpack/manifest.json"; }
 
 old=$(version)
 
-case "$1" in
+case "$2" in
     "major")
-        perl -pi -e 's/(\"version_number\": \")(\d+)\.(\d+)\.(\d+)/$1.join(".", $2+1, 0, 0)/ge' modpack/manifest.json
+        perl -pi -e 's/(\"version_number\": \")(\d+)\.(\d+)\.(\d+)/$1.join(".", $2+1, 0, 0)/ge' "$modpack/manifest.json"
         ;;
     "minor")
-        perl -pi -e 's/(\"version_number\": \")(\d+)\.(\d+)\.(\d+)/$1.join(".", $2, $3+1, 0)/ge' modpack/manifest.json
+        perl -pi -e 's/(\"version_number\": \")(\d+)\.(\d+)\.(\d+)/$1.join(".", $2, $3+1, 0)/ge' "$modpack/manifest.json"
         ;;
-    "patch" | "")
-        perl -pi -e 's/(\"version_number\": \")(\d+)\.(\d+)\.(\d+)/$1.join(".", $2, $3, $4+1)/ge' modpack/manifest.json
+    "patch"|"")
+        perl -pi -e 's/(\"version_number\": \")(\d+)\.(\d+)\.(\d+)/$1.join(".", $2, $3, $4+1)/ge' "$modpack/manifest.json"
         ;;
     *)
-        echo "Usage: $0 [major|minor|patch] (default: patch)"
+        echo "Usage: $0 MODPACK_DIR [major|minor|patch] (default: patch)" >&2;
         exit 1
+        ;;
 esac
 
 echo "$old -> $(version)"
 
-rm modpack.zip || true
-cd modpack
-apack ../modpack.zip ./*
+arc="$(basename "$modpack").zip"
+
+rm "$arc" || true
+cd "$modpack"
+apack "../$arc" ./*
